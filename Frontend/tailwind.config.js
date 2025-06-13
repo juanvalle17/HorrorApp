8b715deb3472e43ed9a06fe0d5d2d6842006c3f9
juanvalle17 <valle.juan.ignacio24@gmail.com>
2025-06-13 16:46:58 -0300

    Instalacion de Tailwind

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/pages/**/*.{html,js}",  // 🟢 HTML
+    "./src/js/**/*.{js}"           // 🟢 JS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
